eda78ab5f74b5e1381653000961739efc6112108
Yong Shean <yshean@gmail.com>
2022-06-22 21:40:55 +0800

    initial commit
    after running `very_good create crew_member_repository -t dart_pkg --desc "A Dart package to manage the crew domain"`

---
+++ lib/src/crew_member_repository.dart
@@ -0,0 +1,7 @@
+/// {@template crew_member_repository}
+/// A Dart package to manage the crew domain
+/// {@endtemplate}
+class CrewMemberRepository {
+  /// {@macro crew_member_repository}
+  const CrewMemberRepository();
+}
---
+++ test/src/crew_member_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:crew_member_repository/crew_member_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CrewMemberRepository', () {
+    test('can be instantiated', () {
+      expect(CrewMemberRepository(), isNotNull);
+    });
+  });
+}
