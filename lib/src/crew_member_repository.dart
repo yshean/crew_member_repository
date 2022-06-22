a141a6858b3aa93627eb5c906b6c2e98e1da6037
Yong Shean <yshean@gmail.com>
2022-06-22 21:47:08 +0800

    add CrewMemberRepository and tests

--- lib/src/crew_member_repository.dart
+++ lib/src/crew_member_repository.dart
@@ -1,7 +1,31 @@
+import 'package:spacex_api/spacex_api.dart';
+
+/// Thrown when an error occurs while looking up the crew members
+class CrewMembersException implements Exception {}
+
 /// {@template crew_member_repository}
 /// A Dart package to manage the crew domain
 /// {@endtemplate}
 class CrewMemberRepository {
-  /// {@macro crew_member_repository}
-  const CrewMemberRepository();
+  ///  {@macro crew_member_repository}
+  CrewMemberRepository({SpaceXApiClient? spaceXApiClient})
+      : _spaceXApiClient = spaceXApiClient ?? SpaceXApiClient();
+
+  final SpaceXApiClient _spaceXApiClient;
+
+  /// Returns an list alphabetabetically sorted by name
+  /// of all SpaceX crew members
+  ///
+  /// Throws a [CrewMembersException] if an error occurs.
+  Future<List<CrewMember>> fetchAllCrewMembers() async {
+    try {
+      final crewMembers = await _spaceXApiClient.fetchAllCrewMembers();
+
+      crewMembers.sort((a, b) => a.name.compareTo(b.name));
+
+      return crewMembers;
+    } on Exception {
+      throw CrewMembersException();
+    }
+  }
 }
